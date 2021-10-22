aa51a1a24e821e248ccc9d401322fcbd9770156f
ArturWolodzko <notoja12@gmail.com>
2021-10-22 13:22:18 +0200

    2 cwiczenia

--- src/Main.java
+++ src/Main.java
@@ -23,9 +23,36 @@ public class Main {
          * wprowadzanymi z klawiatury (użyć klasy Scanner), a następnie będzie wyświetlał elementy tablicy
          * za pomocą pętli forEach powiększone o 11 */
 
-        int[] tab1 = new int[5];
-        Scanner skan = new
+//        int[] tab1 = new int[5];
+//        Scanner skan = new Scanner(System.in);
+//        for (int i=0; i<5 ; i++){
+//            System.out.printf("podaj %d liczbę  " ,i+1);
+//            tab1[i]= skan.nextInt();
+//        }
+////        System.out.printf("%d ,%d ,%d ,%d ,2%d" , tab1[0],tab1[1],tab1[2],tab1[3],tab1[4]);
+//
+//        for (int a:tab1)
+//        { a= a + 11;
+//            System.out.println(a);
 
+        }
 
+    Scanner skan = new Scanner(System.in);
+    String tab1 = new String[5];
+    for (int i = 1; i<5 ; i++)
+
+    {
+        System.out.println("podaj %d imię", i + 1);
+        tab1[i] = skan.nextLine();
     }
+    System.out.printf(%s , %s, %s , %s , )
+
+
+
+
+
+
+
+
+
 }
