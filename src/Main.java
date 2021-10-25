193052bfbb6afbc93985b10e24bfaea48e74e6b0
ArturWolodzko <notoja12@gmail.com>
2021-10-25 11:21:49 +0200

    2 cwiczenia

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,27 @@ public class Main {
           b) program który określi czy liczba jest podzielna przez 7
          */
 
+        int liczba1;
+        Scanner skan = new Scanner(System.in);
+
+        do {
+
+            System.out.println("podaj liczbę");
+            liczba1 = skan.nextInt();
+            switch (liczba1 % 2) {
+                case 0:
+                    System.out.println("liczba jest parzysta");
+                    break;
+                default:
+                    System.out.println("liczba nie parzysta");
+                    break;
+            }
+         System.out.println("czy chcesz powtórzyć jeśli tak wybierz 1");
+        int odpowiedz;
+        odpowiedz = skan.nextInt();}
+
+        while
+
         /* zad.2
            a) napisać program podobny do programu z przykładu, który ma zliczyć iloś liter 'o', 'p', 'x', 'z', 'q' w wyrazie:
             "ooooozjoonoxoooozoooooooooozoxooooopnpppoooiiooooomnzooooxoqoojoozoootyoooozoooxoooozooooxooooooojoooopxoooonnmooo"
