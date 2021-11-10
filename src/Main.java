7e6892f0a02c500236e4402f20feefb08c376727
ArturWolodzko <notoja12@gmail.com>
2021-11-10 12:37:07 +0100

    2 cwiczenia

--- src/Main.java
+++ src/Main.java
@@ -10,17 +10,17 @@ public class Main {
         c) w trzeciej pętli return
         */
 
-//        for (int i = 0  ; i <= 100 ; i++) {
+//        for (int i =  1  ; i <= 100 ; i++) {
 //            if (i == 5)
 //                break;
 //            System.out.println(i +" ");
 //        }
 
-        for (int j = 0 ; j <= 10 ; j++){
-            if (j == 3)
-                continue;
-            System.out.println(j +" ");
-        }
+//        for (int j = 0 ; j <= 10 ; j++){
+//            if (j == 3)
+//                continue;
+//            System.out.println(j +" ");
+//        }
 
         for (int k = 30 ; k <= 40 ; k++){
             if (k == 34)
